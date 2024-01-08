65a0fa3efda4bc6d87e1fb3d7beef4c75613c066
Ayman Alaswad <aymandeveloper0git@gmail.com>
2024-01-08 12:45:52 +0300

    Removing some states is not necessary

--- lib/app/cubit/app_state.dart
+++ lib/app/cubit/app_state.dart
@@ -13,48 +13,12 @@ class ProductsErrorState extends AppStates {}
 
 class CheckConnectionState extends AppStates {}
 
-class CategoriesLoadingState extends AppStates {}
+class ChangeFavoriteState extends AppStates {}
 
-class CategoriesSuccessState extends AppStates {}
-
-class CategoriesErrorState extends AppStates {}
-
-class CategoriesDetailsLoadingState extends AppStates {}
-
-class CategoriesDetailsSuccessState extends AppStates {}
-
-class CategoriesDetailsErrorState extends AppStates {}
-
-class UserCardLoadingState extends AppStates {}
-
-class UserCardSuccessState extends AppStates {}
-
-class UserCardErrorState extends AppStates {}
-
-class LoadingUserDataState extends AppStates {}
-
-class SuccessUserDataState extends AppStates {
-  final UserModel userModel;
-
-  SuccessUserDataState({required this.userModel});
-}
-
-class ErrorUserDataState extends AppStates {}
-
-class ChangeFavoretisState extends AppStates {}
-
-class CreateDatabaseState extends AppStates {}
+class ImagePickerState extends AppStates {}
 
 class InsertDatabaseState extends AppStates {}
 
-class GetDatabaseLoadingState extends AppStates {}
-
-class GetDatabaseState extends AppStates {}
-
-class DeleteDatabaseState extends AppStates {}
-
 class UpdateDatabaseState extends AppStates {}
 
-class IncreaseQuantityState extends AppStates {}
-
-class DecreaseQuantityState extends AppStates {}
+class DeleteDatabaseState extends AppStates {}
