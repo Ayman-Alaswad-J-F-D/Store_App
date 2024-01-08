dd085e01daf2bf41c2c714c50f8339420e5e4e0a
Ayman Alaswad <aymandeveloper0git@gmail.com>
2024-01-08 15:03:07 +0300

    Some modified in card widget

--- lib/widget/custom_circular_progress.dart
+++ lib/widget/custom_circular_progress.dart
@@ -1,31 +1,30 @@
 import 'package:flutter/material.dart';
 
-import '../app/constants.dart';
+import '../theme/colors.dart';
 
 class CustomCircularProgress extends StatelessWidget {
   const CustomCircularProgress({
     Key? key,
     this.elevation = 0.0,
-    this.radius = 0.0,
   }) : super(key: key);
 
   final double elevation;
-  final double radius;
 
   @override
   Widget build(BuildContext context) {
     return Center(
       child: Card(
-        shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(40)),
-        color: kWhiteColor,
+        shape: const CircleBorder(),
+        color: AppColors.whiteColor,
+        surfaceTintColor: AppColors.primaryColor,
         elevation: elevation,
         child: const Padding(
           padding: EdgeInsets.all(8.0),
           child: CircleAvatar(
-            backgroundColor: kWhiteColor,
+            backgroundColor: AppColors.whiteColor,
             child: CircularProgressIndicator(
-              color: kLightPrimaryColor,
-              backgroundColor: kPrimaryColor,
+              color: AppColors.lightPrimaryColor,
+              backgroundColor: AppColors.primaryColor,
             ),
           ),
         ),
@@ -33,14 +32,3 @@ class CustomCircularProgress extends StatelessWidget {
     );
   }
 }
-
-
-// Card(
-//         child: Padding(
-//           padding: EdgeInsets.all(8),
-//           child: CircularProgressIndicator(
-//             backgroundColor: kLightPrimaryColor,
-//             color: kPrimaryColor,
-//           ),
-//         ),
-//       )
