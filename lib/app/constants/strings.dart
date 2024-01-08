575452ef780415bf48b8b8b41fc0761a14dde38e
Ayman Alaswad <aymandeveloper0git@gmail.com>
2024-01-08 10:53:54 +0300

    Added the columns names for database table

---
+++ lib/app/constants/strings.dart
@@ -0,0 +1,12 @@
+const int databaseVersion = 1;
+const String databaseName = "cards.db";
+const String cardsTable = "cards";
+const String colId = "id";
+const String colProductId = "productId";
+const String colTitle = "title";
+const String colDesc = "desc";
+const String colPrice = "price";
+const String colCategory = "category";
+const String colImage = "image";
+const String colQuantity = "quantity";
+const String colDataTime = "dataTime";
