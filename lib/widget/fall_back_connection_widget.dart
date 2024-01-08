5b400f180d6e21c541f07345fc7cf16b05cfe9ba
Ayman Alaswad <aymandeveloper0git@gmail.com>
2024-01-08 13:34:40 +0300

    Enhance widget

--- lib/widget/fall_back_connection_widget.dart
+++ lib/widget/fall_back_connection_widget.dart
@@ -1,21 +1,22 @@
 import 'package:flutter/material.dart';
 
-import '../app/constants.dart';
+import '../app/constants/constants.dart';
+import '../theme/colors.dart';
 import 'custom_button.dart';
 
 class FallBackWidget extends StatelessWidget {
   const FallBackWidget({
     Key? key,
     required this.text,
-    this.colorText = kSecondPrimaryColor,
-    this.needBottom = true,
+    this.colorText = AppColors.secondPrimaryColor,
+    this.colorButton = AppColors.primaryColor,
     this.onTap,
   }) : super(key: key);
 
   final String text;
   final Function()? onTap;
-  final bool needBottom;
   final Color? colorText;
+  final Color colorButton;
 
   @override
   Widget build(BuildContext context) {
@@ -25,21 +26,23 @@ class FallBackWidget extends StatelessWidget {
         children: [
           Text(
             text,
-            style: TextStyle(fontSize: 16, color: colorText),
+            style: TextStyle(
+              fontSize: 16,
+              color: colorText,
+              fontFamily: 'Pacifico',
+            ),
           ),
           const SizedBox(height: 20),
-          needBottom
-              ? Padding(
-                  padding: const EdgeInsets.symmetric(horizontal: 80),
-                  child: CustomButton(
-                    text: "Try again",
-                    height: 10,
-                    backgroundColor: kPrimaryColor,
-                    colorText: kYallowColor,
-                    onTap: onTap!,
-                  ),
-                )
-              : const SizedBox(),
+          if (onTap != null)
+            SizedBox(
+              width: widthScreen(context) / 1.8,
+              child: CustomButton(
+                text: "Try again",
+                backgroundColor: colorButton,
+                colorText: AppColors.yallowColor,
+                onTap: onTap!,
+              ),
+            ),
         ],
       ),
     );
